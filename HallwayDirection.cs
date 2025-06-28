3b7978e26e6d1457ee80ef41b3b014b1e0bf685a
guaaan <juampablo.txt@gmail.com>
2025-06-27 23:05:34 -0400

    indica una salida usando linq

--- HallwayDirection.cs
+++ HallwayDirection.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,6 +16,19 @@ public static class HallwayDirectionExtension
     public static Color GetColor(this HallwayDirection direction)
     {
         return DirectionToColorMap.TryGetValue(direction, out Color color) ? color : Color.gray;
+    }
+
+    public static HallwayDirection GetOppositeDirection(this HallwayDirection direction)
+    {
+        Dictionary<HallwayDirection, HallwayDirection> oppositeDirectionMap = new Dictionary<HallwayDirection, HallwayDirection>
+        {
+            { HallwayDirection.Left, HallwayDirection.Right },
+            { HallwayDirection.Right, HallwayDirection.Left },
+            { HallwayDirection.Top, HallwayDirection.Bottom },
+            { HallwayDirection.Bottom, HallwayDirection.Top }
+        };
+
+        return oppositeDirectionMap.TryGetValue(direction, out HallwayDirection oppositeDirection) ? oppositeDirection : HallwayDirection.Undefined;
     }
 
 }
--- LayoutGeneratorRooms.cs
+++ LayoutGeneratorRooms.cs
@@ -1,53 +1,61 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
+// Clase principal para la generación procedural de layouts de habitaciones en Unity
 public class LayoutGeneratorRooms : MonoBehaviour
 {
+    // Dimensiones generales del nivel
     [SerializeField] int width = 64;
     [SerializeField] int length = 64;
 
+    // Rango de tamaño permitido para las habitaciones
     [SerializeField] int roomWidthMin = 3;
     [SerializeField] int roomWidthMax = 5;
     [SerializeField] int roomLengthMin = 3;
     [SerializeField] int roomLengthMax = 5;
 
+    // Referencia al objeto visualizador del layout y lista de pasillos abiertos
     [SerializeField] GameObject levelLayoutDisplay;
     [SerializeField] List<Hallway> openDoorways;
 
+    // Generador de números aleatorios y referencia al nivel generado
     System.Random random;
     Level level;
 
-    //agrega la función al editor para generar niveles en pausa
+    // Método principal para generar el layout del nivel (se puede ejecutar desde el editor con botón derecho)
     [ContextMenu("Generate Level Layout")]
     public void GenerateLevel()
     {
+        // Inicializa el generador aleatorio y la lista de pasillos abiertos
         random = new System.Random();
         openDoorways = new List<Hallway>();
+        // Crea el objeto Level con las dimensiones especificadas
         level = new Level(width, length);
-
-
+        // Calcula la posición y tamaño de la habitación inicial
         RectInt roomRect = GetStartRoomRect();
         Debug.Log(roomRect);
+        // Crea la habitación inicial y calcula sus posibles pasillos
         Room room = new Room(roomRect);
         List<Hallway> hallways = room.CalculateAllPossibleDoorways(room.Area.width, room.Area.height, 1);
         hallways.ForEach(h => h.StartRoom = room);
         hallways.ForEach(h => openDoorways.Add(h));
+        // Añade la habitación inicial al nivel
         level.AddRoom(room);
 
-        Room testRoom1 = new Room(new RectInt(3, 6, 6, 10));
-        Room testRoom2 = new Room(new RectInt(15, 6, 10, 12));
-        Hallway testHallway = new Hallway(HallwayDirection.Right, new Vector2Int(6, 3), testRoom1);
-        testHallway.EndPosition = new Vector2Int(0, 5);
-        testHallway.EndRoom = testRoom2;
-        level.AddRoom(testRoom1);
-        level.AddRoom(testRoom2);
-        level.AddHallway(testHallway);
+        // Selecciona aleatoriamente un pasillo de entrada y calcula un pasillo de salida compatible
+        Hallway selectedEntryway = openDoorways[random.Next(0, openDoorways.Count)];
+        Hallway selectedExit = SelectHallwayCandidate(new RectInt(0, 0, 5, 7), selectedEntryway);
+        Debug.Log(selectedExit.StartPosition);
+        Debug.Log(selectedExit.StartDirection);
 
-        DrawLayout(roomRect);
+        // Dibuja el layout generado en la textura del visualizador
+        DrawLayout(selectedEntryway, roomRect);
     }
 
+    // Calcula la posición y tamaño de la habitación inicial de forma aleatoria dentro de los límites
     RectInt GetStartRoomRect()
     {
         int roomWidth = random.Next(roomWidthMin, roomWidthMax);
@@ -62,27 +70,42 @@ public class LayoutGeneratorRooms : MonoBehaviour
 
         return new RectInt(roomX, roomY, roomWidth, roomLength);
     }
-    // dibuja el nivel y el ultimo cuarto
-    void DrawLayout(RectInt roomCandidateRect = new RectInt())
+
+    // Dibuja el layout del nivel y resalta la habitación candidata y los pasillos
+    void DrawLayout(Hallway selectedEntryway = null, RectInt roomCandidateRect = new RectInt())
     {
         Renderer renderer = levelLayoutDisplay.GetComponent<Renderer>();
 
-        //TextureArrayContainer del layout
         Texture2D layoutTexture = (Texture2D)renderer.sharedMaterial.mainTexture;
 
-        //tamaño del layout
         layoutTexture.Reinitialize(width, length);
         levelLayoutDisplay.transform.localScale = new Vector3(width, length, 1);
-        layoutTexture.FillWithColor(Color.black); //hacer el fondonegro
+        layoutTexture.FillWithColor(Color.black);
 
-        Array.ForEach(level.Rooms, room => layoutTexture.DrawRectangle(room.Area, Color.white));// dibuja los cuartos
-        Array.ForEach(level.Hallways, hallway => layoutTexture.DrawLine(hallway.StartPositionAbsolute, hallway.EndPositionAbsolute, Color.white)); // dibuja las lineas de los pasillos
-
-        layoutTexture.DrawRectangle(roomCandidateRect, Color.cyan); // dibuja el cuarto
-
+        // Dibuja habitaciones y pasillos existentes
+        Array.ForEach(level.Rooms, room => layoutTexture.DrawRectangle(room.Area, Color.white));
+        Array.ForEach(level.Hallways, hallway => layoutTexture.DrawLine(hallway.StartPositionAbsolute, hallway.EndPositionAbsolute, Color.white));
+        // Dibuja la habitación candidata en azul
+        layoutTexture.DrawRectangle(roomCandidateRect, Color.blue);
+        // Dibuja los posibles pasillos abiertos en su color correspondiente
         openDoorways.ForEach(hallway => layoutTexture.SetPixel(hallway.StartPositionAbsolute.x, hallway.StartPositionAbsolute.y, hallway.StartDirection.GetColor()));
-        //guarda la imagen del layout como sprite
+        // Resalta el pasillo de entrada seleccionado en rojo
+        if (selectedEntryway != null)
+        {
+            layoutTexture.SetPixel(selectedEntryway.StartPositionAbsolute.x, selectedEntryway.StartPositionAbsolute.y, Color.red);
+        }
+
         layoutTexture.SaveAsset();
+    }
+
+    // Selecciona un pasillo candidato para conectar una nueva habitación, filtrando por dirección compatible
+    Hallway SelectHallwayCandidate(RectInt roomCandidateRect, Hallway entryway)
+    {
+        Room room = new Room(roomCandidateRect);
+        List<Hallway> candidates = room.CalculateAllPossibleDoorways(room.Area.width, room.Area.height, 1);
+        HallwayDirection requiredDirection = entryway.StartDirection.GetOppositeDirection();
+        List<Hallway> filteredHallwayCandidates = candidates.Where(hc => hc.StartDirection == requiredDirection).ToList();
+        return filteredHallwayCandidates.Count > 0 ? filteredHallwayCandidates[random.Next(filteredHallwayCandidates.Count)] : null;
     }
 
 }
